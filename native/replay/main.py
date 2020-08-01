7e698b8fd8982cc7fb0a704e88bc734fd7a32a64
patricktelnoni <patricktelnoni@users.noreply.github.com>
2020-08-01 12:25:42 +0700

    Modify schema naming
    Modify sql query to use schema globally from main.py

--- native/replay/main.py
+++ native/replay/main.py
@@ -8,10 +8,11 @@ import hashlib, math
 from operator import concat
 
 conn = psycopg2.connect("host=127.0.0.1 \
-    dbname=c2_viewer_v1 \
+    dbname=shiptrack_kurdi \
     user=postgres \
-    password=LenElhan!@#"
+    password=1234"
 )
+schema_db = "public"
 
 r = redis.Redis(host='localhost', port=6379, db=3)
 
